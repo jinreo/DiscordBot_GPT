f97160e05fd969235cb7e5d62c4ff214ad218b57
Jinreo <jinreo@naver.com>
2024-10-13 10:48:50 +0900

    완성

---
+++ DiscordBot_ChatGPT/bot.py
@@ -0,0 +1,32 @@
+﻿# bot.py
+import os
+import discord
+from dotenv import load_dotenv
+from chatgpt import send_to_chatGpt
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user.name}')
+  channel = client.get_channel(int(CHANNEL_ID))
+  await channel.send("GPT 준비 완료")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    messages = [{"role": "user", "content": message.content}]
+    response = send_to_chatGpt(messages)
+
+    await message.channel.send(response)
+
+# start the bot
+client.run(TOKEN)
---
+++ DiscordBot_ChatGPT/chatgpt.py
@@ -0,0 +1,21 @@
+﻿#chatgpt.py
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+client = OpenAI(api_key=OPENAI_KEY)
+
+
+def send_to_chatGpt(messages, model="gpt-4o"):
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages
+    )
+
+    message = response.choices[0].message.content
+    messages.append(response.choices[0].message)
+    return message
